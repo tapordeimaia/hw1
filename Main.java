dc9f0b14b54f6efeab96c59f21d6e1ba51d145df
tapordeimaia <146013799+tapordeimaia@users.noreply.github.com>
2023-09-30 15:09:18 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        long CurrentWorldPopulation = 7811080815L;
+        int CarsProducedThisYear = 55235358;
+        char FirstLetterInAlphabet = 'A';
+        boolean JavaIsAmazing = true;
+        double Pi = 3.14159;
+        System.out.println(CurrentWorldPopulation);
+        System.out.println(CarsProducedThisYear);
+        System.out.println(FirstLetterInAlphabet);
+        System.out.println(JavaIsAmazing);
+        System.out.println(Pi);
+    }
+}
